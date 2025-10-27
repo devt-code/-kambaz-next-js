93a24c63d3050222241786da25e15c790dfb2905
Deva Sai Sunder Tangella <79849464+devt-code@users.noreply.github.com>
2025-10-27 15:37:04 -0400

    Fixed verecel errors

--- app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
@@ -1,15 +1,28 @@
 "use client";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, Provider } from "react-redux";
 import { useState } from "react";
 import { add } from "./addReducer";
 import { Button, FormControl } from "react-bootstrap";
 import { RootState } from "../../store/store";
+import store from "../../store/store";
 
 export default function AddRedux() {
+  return (
+    <Provider store={store}>
+      <AddReduxInner />
+    </Provider>
+  );
+}
+
+function AddReduxInner() {
   const [a, setA] = useState(12);
   const [b, setB] = useState(23);
-  const { sum } = useSelector((state: RootState) => state.addReducer);
+
+  // Use optional chaining to avoid undefined during server render
+  const sum = useSelector((state: RootState) => state.addReducer?.sum ?? 0);
+
   const dispatch = useDispatch();
+
   return (
     <div className="w-25" id="wd-add-redux">
       <h1>Add Redux</h1>
@@ -18,17 +31,15 @@ export default function AddRedux() {
       </h2>
       <FormControl
         type="number"
-        defaultValue={a}
+        value={a}
         onChange={(e) => setA(parseInt(e.target.value))}
       />
       <FormControl
         type="number"
-        defaultValue={b}
+        value={b}
         onChange={(e) => setB(parseInt(e.target.value))}
       />
-      <Button id="wd-add-redux-click" onClick={() => dispatch(add({ a, b }))}>
-        Add Redux
-      </Button>
+      <Button onClick={() => dispatch(add({ a, b }))}>Add Redux</Button>
       <hr />
     </div>
   );
