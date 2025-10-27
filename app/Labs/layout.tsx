fcea1f30faa5618bc8cdfd1d295bf84284ca66dd
Deva Sai Sunder Tangella <79849464+devt-code@users.noreply.github.com>
2025-10-27 15:26:37 -0400

    Fixed verecel errors

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,19 +1,23 @@
-import { ReactNode } from "react";
-import TOC from "./TOC";
+"use client";
 
-export default function LabsLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+import TOC from "./TOC";
+import store from "../Labs/Lab4/store"; // your Redux store
+
+export default function LabsLayout({ children }: { children: ReactNode }) {
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td valign="top" width="100px">
-            <TOC />
-          </td>
-          <td valign="top">{children}</td>
-        </tr>
-      </tbody>
-    </table>
+    <Provider store={store}>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="100px">
+              <TOC />
+            </td>
+            <td valign="top">{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </Provider>
   );
 }
