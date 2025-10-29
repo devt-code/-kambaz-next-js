1b63b263c146766ad7fc292a511b997c5eaff2b1
Deva Sai Sunder Tangella <79849464+devt-code@users.noreply.github.com>
2025-10-29 15:36:31 -0400

    Fixed verecel errors

--- app/Providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Provider } from "react-redux";
-import { store } from "@/app/Labs/Lab4/store/store"; // adjust path as needed
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
-}
