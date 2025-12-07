6bd19fbbe4dc7989146b1eabe39ec5ddce1e6fae
Rohan Kumar <crohank02@gmail.com>
2025-12-06 23:59:28 -0500

    Update Breadcrumb.tsx

--- app/(Kambaz)/Courses/[cid]/Breadcrumb.tsx
+++ app/(Kambaz)/Courses/[cid]/Breadcrumb.tsx
@@ -8,9 +8,19 @@ export default function Breadcrumb({
   course: { name: string } | undefined;
 }) {
   const pathname = usePathname();
+  const segments = pathname.split("/");
+  const lastSegment = segments.pop() || "";
+  const secondLastSegment = segments[segments.length - 1];
+
+  // If the second-to-last segment is "Assignments" or "Quizzes", use that instead of the UUID
+  const displaySegment =
+    secondLastSegment === "Assignments" || secondLastSegment === "Quizzes"
+      ? secondLastSegment
+      : lastSegment;
+
   return (
     <span>
-      {course?.name} &gt; {pathname.split("/").pop()}
+      {course?.name} &gt; {displaySegment}
     </span>
   );
 }
